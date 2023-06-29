b373f7dab19a0ab0a33774eb38d9d52bfd063f0f
Christopher Karwin <ckarwin@login003.palmetto.clemson.edu>
2023-06-29 13:40:10 -0400

    added example notebook

--- examples/background_from_aspect/README.txt
+++
@@ -1,3 +0,0 @@
-This is a simple example of how to easily calculate a time-dependent atmoshpere model from 
-an instrument's aspect information. The next step would be to use this to make a
-time-dependent background model (e.g. Ling). An atmospheric background class will be added soon. 
---
+++ examples/scripts/standard_atmosphere_sims/run_atm_sims.py
@@ -0,0 +1,35 @@
+# Imports:
+from cosi_atmosphere.response.AtmosphericProfile import Atmosphere
+from cosi_atmosphere.response.MassModels import MakeMassModels
+from cosi_atmosphere.response.RunSims import Simulate 
+from cosi_atmosphere.response.ProcessSims import Process
+import numpy as np
+
+# Get Atmospheric model:
+#instance = Atmosphere()
+#date = np.array(['2016-06-13 12:00:00'], dtype="datetime64[h]")
+#lat = -5.66
+#lon = -107.38
+#alts = np.linspace(0, 200, 2001) # km; spacing is 0.1 km (100 m) 
+#atm_model = instance.get_atm_profile("rep_atm_model.dat",date,lon,lat,alts)
+
+# Make mass model:
+#instance = MakeMassModels("rep_atm_model.dat")
+#instance.plot_atmosphere()
+#instance.get_cart_vectors(angle, altitude)
+#instance.rectangular_model(33)
+#instance.spherical_model(33)
+
+# Run sim:
+#instance = Simulate()
+#instance.run_sim("AtmospherePencilBeam.source", seed=3000)
+#instance.parse_sim_file("Atmosphere_PencilBeam.inc1.id1.sim")
+
+# Process sim:
+#instance = Process("rectangular",0)
+#instance.bin_sim()
+#instance.make_scattering_plots()
+#instance.calc_tp()
+#instance.get_total_edisp_matrix()
+#model_flux=instance.PL_interp(2)
+#instance.ff_correction(model_flux)
