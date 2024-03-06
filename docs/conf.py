3f6fe92f9fb5cfc2facfb172584d7ffd56aef48c
Chris Karwin <54562666+ckarwin@users.noreply.github.com>
2024-03-05 22:23:42 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,7 @@ autodoc_mock_imports = ['pymsis',
                         'histpy',
                         'astropy',
                         'healpy',
+                        'mhealpy'
                         'scipy']
 
 # Add any paths that contain templates here, relative to this directory.
