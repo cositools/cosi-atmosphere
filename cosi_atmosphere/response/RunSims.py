8ebf54235ce0776ec0701065c78c9ffcbbcbc507
Chris Karwin <54562666+ckarwin@users.noreply.github.com>
2024-07-05 16:24:20 -0400

    Update RunSims.py
    removed colorcet dependency.

--- cosi_atmosphere/response/RunSims.py
+++ cosi_atmosphere/response/RunSims.py
@@ -3,7 +3,6 @@ import pandas as pd
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-import colorcet as cc
 from histpy import Histogram
 import gc 
 from astropy.convolution import convolve, Gaussian2DKernel
