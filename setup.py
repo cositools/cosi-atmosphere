1236ffd0e745fac7196ccc41e93bd654ef08458a
Chris Karwin <54562666+ckarwin@users.noreply.github.com>
2024-07-05 16:35:30 -0400

    Update setup.py
    removed colorcet dependence.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 # Setup:
 setup(
     name='cosi_atmosphere',
-    version="0.1.6",
+    version="0.1.7",
     url='https://github.com/cositools/cosi-atmosphere.git',
     author='COSI Team',
     author_email='christopher.m.karwin@nasa.gov',
